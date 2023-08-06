39fbee77d35d8537f3c06d8228552e5b464b051e
Abraham Audu <ab6ril@gmail.com>
2023-08-06 02:00:05 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An app which allows users to get an estimated Solar PV system design specification and price esimate based on inputed energy needs.',
+    author='Abraham Audu--Periva Energy Systems',
+    license='MIT',
+)
